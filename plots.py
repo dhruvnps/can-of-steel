a3d94021bb1cf068e8f6a820e4616947fde2a573
dhruvpattem <dhruv.pattem@gmail.com>
2021-03-09 20:29:13 +0000

    conc at 500 vs 3 day time

--- plots.py
+++ plots.py
@@ -30,31 +30,31 @@ for var in variables:
         ds = Dataset(var)
 
         fig, ax = plt.subplots()
-        fig.suptitle(var)
-        # fig.suptitle(var + '⁻¹')
-        ax.set_xlabel('altitude / m')
-        ax.set_ylabel('conc / µg/m3')
-        # ax.set_ylabel('conc⁻¹ / m3/µg')
+        # fig.suptitle(var + ' at 500m')
+        fig.suptitle(var + '⁻¹ at 500m')
+        ax.set_xlabel('time / hrs (starts midnight)')
+        # ax.set_ylabel('conc / µg/m3')
+        ax.set_ylabel('conc⁻¹ / m3/µg')
         plt.subplots_adjust(left=.15, right=.95, bottom=.1, top=.9)
 
-        hours = 24
+        hours = 1
         for time in range(hours):
 
             # levels = ds.levels
-            levels = ds.levels[:4]
+            # levels = ds.levels[:4]
 
             data = [
-                ds.data[time][idx][LAT_idx][LNG_idx]
-                for idx, _ in enumerate(levels)
+                1/ds.data[idx][4][LAT_idx][LNG_idx]
+                for idx, _ in enumerate(ds.times)
             ]
 
-            pct = time / (hours - 1)
-            p, = plt.plot(levels, data, color=color(pct))
-            # p, = plt.plot(ds.levels, data, color='red')
+            #pct = time / (hours - 1)
+            #p, = plt.plot(ds.times, data, color=color(pct))
+            p, = plt.plot(ds.times, data, color='red')
 
             custom_lines = [Line2D([0], [0], color=min_color, lw=4),
                             Line2D([0], [0], color=color(0.5), lw=4),
                             Line2D([0], [0], color=max_color, lw=4)]
-            ax.legend(custom_lines, ['0 hrs', '12 hrs', '24 hrs'])
+            # ax.legend(custom_lines, ['0 hrs', '12 hrs', '24 hrs'])
 
-        plt.savefig('figures/plots/time-variant/basic/500/' + var)
+        plt.savefig('figures/plots/altitude-500/reciprocal/' + var)
