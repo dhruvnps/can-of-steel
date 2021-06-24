e02ead59b94b614b202b1d99bc1414b3b79614c6
Dhruv Raja <59893449+dhruvnps@users.noreply.github.com>
2021-06-24 18:48:02 +0100

    Update heatmaps.py

--- predictions/heatmaps.py
+++ predictions/heatmaps.py
@@ -24,7 +24,6 @@ for var in variables:
         fig, ax = plt.subplots()
         fig.suptitle('London - ' + var)
 
-        # plt.subplots_adjust(left=.1, right=.9, bottom=.3, top=.9)
         plt.subplots_adjust(left=.1, right=.9, bottom=.2, top=.9)
 
         ax.set_xlabel('latitude')
@@ -57,39 +56,3 @@ for var in variables:
             writer = animation.PillowWriter(fps=FPS)
 
         anim.save('figures/heatmaps/' + var + filetype, writer=writer)
-
-
-# def update(val):
-#     level.valtext.set_text(ds.levels[int(level.val)])
-#     time.valtext.set_text(ds.times[int(time.val)])
-#     global c
-#     for coll in c.collections:
-#         coll.remove()
-#     c = ax.contourf(
-#         ds.data[int(time.val)][int(level.val)],
-#         vmin=ds.vmin,
-#         vmax=ds.vmax,
-#     )
-
-
-# level = Slider(
-#     plt.axes([.1, .1, .8, .03]),
-#     'altitude',
-#     valmin=0,
-#     valmax=len(ds.levels) - 1,
-#     valstep=1,
-#     valinit=0,
-# )
-# level.on_changed(update)
-
-# time = Slider(
-#     plt.axes([.1, .15, .8, .03]),
-#     'time',
-#     valmin=0,
-#     valmax=len(ds.times) - 1,
-#     valstep=1,
-#     valinit=0,
-# )
-# time.on_changed(update)
-
-# plt.show()
